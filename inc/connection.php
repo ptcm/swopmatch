21a0115342d64f6dcc97d51d217b57c73ba8f9a9
Partson <pchizema@gmail.com>
2019-03-15 15:47:58 +0200

    login credentials for connection and logo

--- inc/connection.php
+++ inc/connection.php
@@ -2,8 +2,10 @@
 	
 	try {
 	    $dsn = 'mysql:host=localhost;dbname=swopmatc_sMatch';
-	        $username = 'swopmatc_swopmat';
-			$password = 'swopenter1';
+	        //$username = 'swopmatc_swopmat';
+			//$password = 'swopenter1';
+	        $username = 'root';
+			$password = '';
 
 			$db = new PDO($dsn, $username, $password);
 			$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
